ff8038b18b196c2774bad68fcbf81f68f530aa9b
kabir <you@example.com>
2018-06-10 01:43:03 +0600

    Mastering

---
+++ resources/views/frontEnd/menu.blade.php
@@ -0,0 +1,15 @@
+<div style="color:red;">
+            <nav class="navbar navbar-light navbar-expand-md navigation-clean-button">
+                <div class="container"><a class="navbar-brand" href="#">Online Exam</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse"
+                         id="navcol-1">
+                        <ul class="nav navbar-nav mr-auto">
+                            <li class="nav-item" role="presentation"><a class="nav-link active" href="#">Rules</a></li>
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="#">Why Here</a></li>
+                            <li class="dropdown"><a class="dropdown-toggle nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">Choose a Subject </a>
+                                <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">CSE</a><a class="dropdown-item" role="presentation" href="#">Networking</a><a class="dropdown-item" role="presentation" href="#">BBA</a></div>
+                            </li>
+                        </ul><span class="navbar-text actions"> <a href="#" class="login">Log In</a><a class="btn btn-light action-button" role="button" href="#">Sign Up</a></span></div>
+                </div>
+            </nav>
+        </div>
